c8997d69937387a225287a22b25edc444b6809b0
Noliani Clemente <noliani.clemente@protonmail.com>
2026-03-27 11:08:25 -0500

    Add tooltips with date formats to table headers

--- src/components/EventsListTable.vue
+++ src/components/EventsListTable.vue
@@ -8,6 +8,45 @@
   import { useEventsListStore } from '@/stores/eventsList';
   const events_list_store = useEventsListStore ()
 
+  import { useDate } from 'vuetify'
+  const date_store = useDate()
+
+  const this_year = new Date ().getFullYear ()
+  const this_year_two_digit = this_year.toString ().substring(2)
+
+  const accepted_date_time_formats = `<h4>Accepted date & time formats</h4>
+    <br />
+    <h6>Dates</h6>
+    <ul class="ml-4">
+      <li>${ date_store.format( new Date (`03/07/${ this_year }`), 'weekday' ) }, 07 March ${ this_year }</li>
+      <li>${ date_store.format( new Date (`03/07/${ this_year }`), 'weekdayShort' ) } 7 March ${ this_year_two_digit }</li>
+      <li>7-Mar-${ this_year }</li>
+      <li>07 Mar ${ this_year_two_digit }</li>
+      <li>Mar7,${ this_year_two_digit }</li>
+      <li>Mar 7</li>
+      <li>7 Mar</li>
+      <li>March 7 ${ this_year }</li>
+      <li>Mar 7, ${ this_year }</li>
+      <li>${ this_year }-03-07</li>
+      <li>03-7-${ this_year_two_digit }</li>
+      <li>${ this_year }/3/7</li>
+      <li>3/07/${ this_year }</li>
+      <li>03/7/${ this_year_two_digit }</li>
+      <li>7/3</li>
+      <li>${ this_year }0307</li>
+    </ul>
+    <br />
+    <h6>Times</h6>
+    <ul class="ml-4">
+      <li>9:26 PM</li>
+      <li>9:26 p.m.</li>
+      <li>9:26pm</li>
+      <li>9pm</li>
+      <li>9p</li>
+      <li>21</li>
+      <li>21:26</li>
+    </ul>`
+
   const headers = [
     {
       title: 'Title',
@@ -20,12 +59,14 @@
       key: 'start',
       value: 'start.dateTime',
       width: '17rem',
+      hint: accepted_date_time_formats,
     },
     {
       title: 'End Date/Time',
       key: 'end',
       value: 'end.dateTime',
       width: '17rem',
+      hint: accepted_date_time_formats,
     },
     {
       title: 'Location',
@@ -57,6 +98,28 @@
           height="calc( 80vh - 4rem)"
           :loading="events_list_store.searching_events || events_list_store.loading_more_events"
           >
+          <template #headers="{ headers }">
+            <th v-for="header in headers [0]" :key="header.key"
+              :style="{
+                width: header.width || 'unset',
+              }">
+              {{ header.title }}
+              <VIcon
+                v-if="header.hint"
+                :id="`${ header.key }_hint`"
+                icon="mdi-information-variant-circle-outline"
+                color="blue-grey-lighten-4"
+                ></VIcon>
+                <VTooltip
+                  :activator="`#${ header.key }_hint`"
+                  >
+                  <div
+                    class="mt-4 mb-2"
+                    v-html="header.hint"
+                    ></div>
+                </VTooltip>
+            </th>
+          </template>
           <template v-slot:item="{ item, itemRef }">
             <EventsListRow
               :key="item.value.key"
